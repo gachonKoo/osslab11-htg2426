e1a966f8d2e44240b044833942c20bc44a4aa9b2
htg2426 <ted2426@naver.com>
2024-11-17 19:58:22 +0900

    Delete tester.py

--- tester.py
+++
@@ -1,8 +0,0 @@
-from geo.utils import calculate_circle_area
-
-def test_calculate_circle_area():
-    assert calculate_circle_area(1) == 3.141592653589793, "Test failed for radius 1"
-    print("Test passed!")
-
-if __name__ == "__main__":
-    test_calculate_circle_area()
